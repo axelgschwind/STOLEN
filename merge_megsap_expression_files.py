5f1a68bd18eb3d3e022a27c366cd4013ac739093
axelgschwind <axel.gschwind@t-online.de>
2024-04-09 14:08:36 +0200

    merge_megsap_expression_files.py: added option for metadata

--- merge_megsap_expression_files.py
+++ merge_megsap_expression_files.py
@@ -16,10 +16,21 @@ def main(argv):
 	parser = OptionParser(usage=usage, description=desc)
 	parser.add_option("-o", "--out", action="store", dest="out", type="string", help="Output merged TSV")
 	parser.add_option("--column", action="store", dest="column", type="string", default="tpm", help="Column from input file to merge")
+	parser.add_option("--paths", action="store", dest="paths", type="string", default= "-", help="File containing paths.")
+	parser.add_option("--metadata", action="store", dest="metadata", type="string", default= "-", help="metadata file.")
 	(options, args) = parser.parse_args()
 
 	merged_df = pd.DataFrame()
-	for file in args:
+ 
+	paths = []
+	if options.paths == "-":
+		for file in args:
+			paths.append(file)
+	else:
+		with open(options.paths, "r") as f:
+			paths = f.read().splitlines()
+
+	for file in paths:
 		if not os.path.isfile(file):
 			continue
 		
@@ -34,6 +45,12 @@ def main(argv):
 		merged_df = merged_df.merge(data, how="outer", left_index=True, right_index=True)
 
 	merged_df.dropna(axis=0, inplace=True)
+ 
+	if options.metadata != "-":
+		mf = pd.read_csv(options.metadata, delimiter="\t")
+		for col in merged_df.columns:
+			if not col in mf["RNA"].tolist():
+				merged_df.drop(columns=[col], axis=1, inplace=True)
 
 	merged_df.to_csv(options.out, sep="\t")
 
